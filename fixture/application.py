1dff92281eb75de434880bdeaf00988d3f0636cc
sergei_sh <pazoviy@ayndex.ru>
2020-08-07 02:41:04 +0300

    Урок 9. Видео "5_ПДТ-Python-b12 Simple Object Access Protocol (SOAP)" 1) В классе Application добавлен помощник: SoapHelper; 2) В фикстуле создали новых помощника SoapHelper; 3) модифицировали тест регистрации нового аккаунта через SOAP;

--- fixture/application.py
+++ fixture/application.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.select import Select
 from fixture.james import JamesHelper
 from fixture.signup import SignupHelper
 from fixture.mail import MailHelper
+from fixture.soap import SoapHelper
 
 
 class Application:
@@ -26,6 +27,7 @@ class Application:
           self.signup = SignupHelper(self)
           self.mail = MailHelper(self)
           self.james = JamesHelper(self)
+          self.soap = SoapHelper(self)
           self.config = config
           self.base_url = config["web"]["baseUrl"]
 
