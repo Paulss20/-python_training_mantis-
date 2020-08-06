96b56cdf2a5c01b2473b4972f0b4f3bdeedc3248
sergei_sh <pazoviy@ayndex.ru>
2020-08-07 02:07:31 +0300

    Урок 9. Видео "4_ПДТ-Python-b12 Получение почты по протоколу POP3" 1) В классе Application добавили два помощника: SignupHelper и MailHelper; 2) В файле "config_inc.php" изменили строку для отключения кэпчи "$g_use_captcha = OFF;" на новую строку - "$g_signup_use_captcha = OFF;" 3) модифицировали тест регистрации нового аккаунта и там же добавили генерацию случайного пользователя; 4) В фикстуле создали новых помощников - SignupHelper и MailHelper;

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,8 @@ from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from selenium.webdriver.support.select import Select
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
@@ -21,6 +23,8 @@ class Application:
           self.wd.implicitly_wait(5) # t.k. stranica obnovlayetsay ne dinamicheski, to v etom priloghenii takoi neobhodimosti net. Alexei razreshil ostavit
           self.session = SessionHelper(self)
           self.project = ProjectHelper(self)
+          self.signup = SignupHelper(self)
+          self.mail = MailHelper(self)
           self.james = JamesHelper(self)
           self.config = config
           self.base_url = config["web"]["baseUrl"]
